c46c79044dc93020bba0cddb9d952be0645f5811
gioadami <giordano.adami@gmail.com>
2017-06-01 16:51:27 +0200

    Exception handler.

--- src/main/java/it/smartcommunitylab/aac/controller/BasicProfileController.java
+++ src/main/java/it/smartcommunitylab/aac/controller/BasicProfileController.java
@@ -19,17 +19,21 @@ package it.smartcommunitylab.aac.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import it.smartcommunitylab.aac.dto.AccountProfile;
 import it.smartcommunitylab.aac.dto.AccountProfiles;
@@ -37,6 +41,7 @@ import it.smartcommunitylab.aac.dto.BasicProfile;
 import it.smartcommunitylab.aac.dto.BasicProfiles;
 import it.smartcommunitylab.aac.manager.BasicProfileManager;
 import it.smartcommunitylab.aac.manager.UserManager;
+import it.smartcommunitylab.aac.model.ErrorInfo;
 
 /**
  * @author raman
@@ -46,115 +51,76 @@ import it.smartcommunitylab.aac.manager.UserManager;
 public class BasicProfileController {
 
 	private Log logger = LogFactory.getLog(getClass());
-	@Autowired 
+	@Autowired
 	private UserManager userManager;
-	
-	
+
 	@Autowired
 	private BasicProfileManager profileManager;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/basicprofile/all/{userId}")
-	public @ResponseBody
-	BasicProfile getUser(HttpServletResponse response,
-			@PathVariable("userId") String userId) throws IOException {
-		try {
-			return profileManager.getBasicProfileById(userId);
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return null;
-		}
-
+	public @ResponseBody BasicProfile getUser(HttpServletResponse response, @PathVariable("userId") String userId)
+			throws IOException {
+		return profileManager.getBasicProfileById(userId);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/basicprofile/all")
-	public @ResponseBody
-	BasicProfiles searchUsers(
-			HttpServletResponse response,
-			@RequestParam(value = "filter", required = false) String fullNameFilter)
-			throws IOException {
+	public @ResponseBody BasicProfiles searchUsers(HttpServletResponse response,
+			@RequestParam(value = "filter", required = false) String fullNameFilter) throws IOException {
+		List<BasicProfile> list;
+		if (fullNameFilter != null && !fullNameFilter.isEmpty()) {
+			list = profileManager.getUsers(fullNameFilter);
 
-		try {
-			List<BasicProfile> list;
-			if (fullNameFilter != null && !fullNameFilter.isEmpty()) {
-				list = profileManager.getUsers(fullNameFilter);
-
-			} else {
-				list = profileManager.getUsers();
-			}
-
-			BasicProfiles profiles = new BasicProfiles();
-			profiles.setProfiles(list);
-			return profiles;
-
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return null;
+		} else {
+			list = profileManager.getUsers();
 		}
+
+		BasicProfiles profiles = new BasicProfiles();
+		profiles.setProfiles(list);
+		return profiles;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/basicprofile/me")
-	public @ResponseBody
-	BasicProfile findProfile(HttpServletResponse response)
-			throws IOException {
-		try {
-			Long user = userManager.getUserId();
-			if (user == null) {
-				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-				return null;
-			}
-			return profileManager.getBasicProfileById(user.toString());
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	public @ResponseBody BasicProfile findProfile(HttpServletResponse response) throws IOException {
+		Long user = userManager.getUserId();
+		if (user == null) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 			return null;
 		}
+		return profileManager.getBasicProfileById(user.toString());
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/basicprofile/profiles")
-	public @ResponseBody
-	BasicProfiles findProfiles(HttpServletResponse response, @RequestParam List<String> userIds) {
-		try {
-			BasicProfiles profiles = new BasicProfiles();
-			profiles.setProfiles(profileManager.getUsers(userIds));
-			return profiles;
-		} catch (Exception e) {
-			logger.error(e);
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return null;
-		}
+	public @ResponseBody BasicProfiles findProfiles(HttpServletResponse response, @RequestParam List<String> userIds) {
+		BasicProfiles profiles = new BasicProfiles();
+		profiles.setProfiles(profileManager.getUsers(userIds));
+		return profiles;
 	}
 
-
 	@RequestMapping(method = RequestMethod.GET, value = "/accountprofile/me")
-	public @ResponseBody
-	AccountProfile findAccountProfile(HttpServletResponse response)
-			throws IOException {
-		try {
-			Long user = userManager.getUserId();
-			if (user == null) {
-				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-				return null;
-			}
-			return profileManager.getAccountProfileById(user.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	public @ResponseBody AccountProfile findAccountProfile(HttpServletResponse response) throws IOException {
+		Long user = userManager.getUserId();
+		if (user == null) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 			return null;
 		}
+		return profileManager.getAccountProfileById(user.toString());
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/accountprofile/profiles")
-	public @ResponseBody
-	AccountProfiles findAccountProfiles(HttpServletResponse response, @RequestParam List<String> userIds)
-			throws IOException {
-		try {
-			AccountProfiles profiles = new AccountProfiles();
-			profiles.setProfiles(profileManager.getAccountProfilesById(userIds));
-			return profiles;
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return null;
-		}
+	public @ResponseBody AccountProfiles findAccountProfiles(HttpServletResponse response,
+			@RequestParam List<String> userIds) throws IOException {
+		AccountProfiles profiles = new AccountProfiles();
+		profiles.setProfiles(profileManager.getAccountProfilesById(userIds));
+		return profiles;
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	ErrorInfo handleBadRequest(HttpServletRequest req, Exception ex) {
+		StackTraceElement ste = ex.getStackTrace()[0];
+		return new ErrorInfo(req.getRequestURL().toString(), ex.getClass().getTypeName(), ste.getClassName(),
+				ste.getLineNumber());
 	}
 
 }
--- src/main/java/it/smartcommunitylab/aac/controller/RolesController.java
+++ src/main/java/it/smartcommunitylab/aac/controller/RolesController.java
@@ -27,11 +27,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.google.common.base.Splitter;
 
@@ -39,6 +41,7 @@ import it.smartcommunitylab.aac.Config.ROLE_SCOPE;
 import it.smartcommunitylab.aac.manager.RoleManager;
 import it.smartcommunitylab.aac.manager.UserManager;
 import it.smartcommunitylab.aac.model.ClientDetailsEntity;
+import it.smartcommunitylab.aac.model.ErrorInfo;
 import it.smartcommunitylab.aac.model.Role;
 import it.smartcommunitylab.aac.model.User;
 import it.smartcommunitylab.aac.repository.ClientDetailsRepository;
@@ -50,158 +53,142 @@ public class RolesController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private RoleManager roleManager;
-	@Autowired 
-	private UserManager userManager;	
+	@Autowired
+	private UserManager userManager;
 	@Autowired
 	UserManagementService userManagementService;
 	@Autowired
-	private ResourceServerTokenServices resourceServerTokenServices;	
+	private ResourceServerTokenServices resourceServerTokenServices;
 	@Autowired
-	private ClientDetailsRepository clientDetailsRepository;	
-	
+	private ClientDetailsRepository clientDetailsRepository;
+
 	@RequestMapping(method = RequestMethod.GET, value = "/userroles/me")
-	public @ResponseBody
-	Set<Role> getRoles(HttpServletResponse response) throws Exception {
-		try {
-			Long userId = userManager.getUserId();
-			if (userId == null) {
-				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-				return null;
-			}
-			User user = userRepository.findOne(userId);
-			
-			return user.getRoles();
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	public @ResponseBody Set<Role> getRoles(HttpServletResponse response) throws Exception {
+		Long userId = userManager.getUserId();
+		if (userId == null) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 			return null;
 		}
+		User user = userRepository.findOne(userId);
+
+		return user.getRoles();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.PUT, value = "/userroles/user/{userId}")
-	public @ResponseBody
-	void addRoles(HttpServletRequest request, HttpServletResponse response, @PathVariable Long userId, @RequestParam String roles) throws Exception {
-		try {
-			User user = userRepository.findOne(userId);
-			if (user == null) {
-				response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
-				return;	
-			}
-			
-			String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
-			OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
-			String clientId = auth.getOAuth2Request().getClientId();
-			ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
-			Long developerId = client.getDeveloperId();
-			
-			User developer = userRepository.findOne(developerId);
-			Role provider = developer.getRoles().stream().filter(x -> "ROLE_PROVIDER".equals(x.getRole())).findFirst().get();
-			String tenant = provider.getContext();			
-			
-			Set<Role> fullRoles = Splitter.on(",").splitToList(roles).stream().map(x -> new Role(ROLE_SCOPE.application, x, tenant)).collect(Collectors.toSet());
-			user.getRoles().addAll(fullRoles);
-			
-			userRepository.save(user);
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	public @ResponseBody void addRoles(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable Long userId, @RequestParam String roles) throws Exception {
+		User user = userRepository.findOne(userId);
+		if (user == null) {
+			response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
+			return;
 		}
-	}		
-	
+
+		String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
+		OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
+		String clientId = auth.getOAuth2Request().getClientId();
+		ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
+		Long developerId = client.getDeveloperId();
+
+		User developer = userRepository.findOne(developerId);
+		Role provider = developer.getRoles().stream().filter(x -> "ROLE_PROVIDER".equals(x.getRole())).findFirst()
+				.get();
+		String tenant = provider.getContext();
+
+		Set<Role> fullRoles = Splitter.on(",").splitToList(roles).stream()
+				.map(x -> new Role(ROLE_SCOPE.application, x, tenant)).collect(Collectors.toSet());
+		user.getRoles().addAll(fullRoles);
+
+		userRepository.save(user);
+	}
+
 	@RequestMapping(method = RequestMethod.DELETE, value = "/userroles/user/{userId}")
-	public @ResponseBody
-	void deleteRoles(HttpServletRequest request, HttpServletResponse response, @PathVariable Long userId, @RequestParam String roles) throws Exception {
-		try {
-			User user = userRepository.findOne(userId);
-			if (user == null) {
-				response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
-				return;
-			}
-			
-			String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
-			OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
-			String clientId = auth.getOAuth2Request().getClientId();
-			ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
-			Long developerId = client.getDeveloperId();
-			
-			User developer = userRepository.findOne(developerId);
-			Role provider = developer.getRoles().stream().filter(x -> "ROLE_PROVIDER".equals(x.getRole())).findFirst().get();
-			String tenant = provider.getContext();			
-			
-			Set<Role> fullRoles = Splitter.on(",").splitToList(roles).stream().map(x -> new Role(ROLE_SCOPE.application, x, tenant)).collect(Collectors.toSet());
-			user.getRoles().removeAll(fullRoles);
-			
-			userRepository.save(user);
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	public @ResponseBody void deleteRoles(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable Long userId, @RequestParam String roles) throws Exception {
+		User user = userRepository.findOne(userId);
+		if (user == null) {
+			response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
+			return;
 		}
-	}		
-	
+
+		String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
+		OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
+		String clientId = auth.getOAuth2Request().getClientId();
+		ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
+		Long developerId = client.getDeveloperId();
+
+		User developer = userRepository.findOne(developerId);
+		Role provider = developer.getRoles().stream().filter(x -> "ROLE_PROVIDER".equals(x.getRole())).findFirst()
+				.get();
+		String tenant = provider.getContext();
+
+		Set<Role> fullRoles = Splitter.on(",").splitToList(roles).stream()
+				.map(x -> new Role(ROLE_SCOPE.application, x, tenant)).collect(Collectors.toSet());
+		user.getRoles().removeAll(fullRoles);
+
+		userRepository.save(user);
+	}
+
 	@RequestMapping(method = RequestMethod.GET, value = "/userroles/all/user/{userId}")
-	public @ResponseBody
-	Set<Role> getAllRoles(HttpServletResponse response, @PathVariable Long userId) throws Exception {
-		try {
-			User user = userRepository.findOne(userId);
-			if (user == null) {
+	public @ResponseBody Set<Role> getAllRoles(HttpServletResponse response, @PathVariable Long userId)
+			throws Exception {
+		User user = userRepository.findOne(userId);
+		if (user == null) {
 			response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
 			return null;
-		}			
-			
-			return user.getRoles();
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return null;
 		}
-	}	
-	
-	
+		return user.getRoles();
+	}
+
 	@RequestMapping(method = RequestMethod.GET, value = "/userroles/user/{userId}")
-	public @ResponseBody
-	Set<Role> getRoles(HttpServletRequest request, HttpServletResponse response, @PathVariable Long userId) throws Exception {
-		try {
-			User user = userRepository.findOne(userId);
-			if (user == null) {
-				response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
-				return null;	
-			}
-			
-			String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
-			OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
-			String clientId = auth.getOAuth2Request().getClientId();
-			ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
-			Long developerId = client.getDeveloperId();
-			
-			User developer = userRepository.findOne(developerId);
-			Role provider = developer.getRoles().stream().filter(x -> "ROLE_PROVIDER".equals(x.getRole())).findFirst().get();
-			String tenant = provider.getContext();			
-			
-			Set<Role> roles = user.getRoles().stream().filter(x -> tenant.equals(x.getContext())).collect(Collectors.toSet());
-			
-			return roles;
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	public @ResponseBody Set<Role> getRoles(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable Long userId) throws Exception {
+
+		User user = userRepository.findOne(userId);
+		if (user == null) {
+			response.sendError(HttpStatus.NOT_ACCEPTABLE.value(), "User " + userId + " not found.");
 			return null;
 		}
-	}		
-	
+
+		String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
+		OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
+		String clientId = auth.getOAuth2Request().getClientId();
+		ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
+		Long developerId = client.getDeveloperId();
+
+		User developer = userRepository.findOne(developerId);
+		Role provider = developer.getRoles().stream().filter(x -> "ROLE_PROVIDER".equals(x.getRole())).findFirst()
+				.get();
+		String tenant = provider.getContext();
+
+		Set<Role> roles = user.getRoles().stream().filter(x -> tenant.equals(x.getContext()))
+				.collect(Collectors.toSet());
+
+		return roles;
+	}
+
 	@RequestMapping(method = RequestMethod.GET, value = "/userroles/client")
-	public @ResponseBody
-	Set<Role> getClientRoles(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		try {
+	public @ResponseBody Set<Role> getClientRoles(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
 			String parsedToken = it.smartcommunitylab.aac.common.Utils.parseHeaderToken(request);
 			OAuth2Authentication auth = resourceServerTokenServices.loadAuthentication(parsedToken);
 			String clientId = auth.getOAuth2Request().getClientId();
 			ClientDetailsEntity client = clientDetailsRepository.findByClientId(clientId);
 			Long developerId = client.getDeveloperId();
-			
+
 			User developer = userRepository.findOne(developerId);
-			
+
 			return developer.getRoles();
-		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			return null;
-		}
-	}	
-	
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	ErrorInfo handleBadRequest(HttpServletRequest req, Exception ex) {
+		StackTraceElement ste = ex.getStackTrace()[0];
+		return new ErrorInfo(req.getRequestURL().toString(), ex.getClass().getTypeName(), ste.getClassName(), ste.getLineNumber());
+	}
+
 }
