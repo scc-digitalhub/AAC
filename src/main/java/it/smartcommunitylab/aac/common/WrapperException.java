b9b6c8ed2ceefc79480c1e51e12feb14653d9a3e
gioadami <giordano.adami@gmail.com>
2017-09-25 12:56:48 +0200

    Wrapper exception.

---
+++ src/main/java/it/smartcommunitylab/aac/common/WrapperException.java
@@ -0,0 +1,9 @@
+package it.smartcommunitylab.aac.common;
+
+public class WrapperException extends Exception {
+
+	public WrapperException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
