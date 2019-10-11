d1446e8f10a902e17a19d0f302d627e9e0ef994f
Matteo Saloni <matteo.saloni@gmail.com>
2019-10-11 17:42:22 +0200

    add JWT encoding for Oauth2 access tokens, include custom claims and scopes, also include custom converter and formatter as experimental code

---
+++ src/main/java/it/smartcommunitylab/aac/oauth/AACJwtTokenConverter.java
@@ -0,0 +1,432 @@
+package it.smartcommunitylab.aac.oauth;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
+import org.springframework.security.jwt.crypto.sign.RsaSigner;
+import org.springframework.security.jwt.crypto.sign.Signer;
+import org.springframework.security.oauth2.common.DefaultExpiringOAuth2RefreshToken;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.DefaultOAuth2RefreshToken;
+import org.springframework.security.oauth2.common.ExpiringOAuth2RefreshToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
+import org.springframework.security.oauth2.common.util.JsonParser;
+import org.springframework.security.oauth2.common.util.JsonParserFactory;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.google.common.collect.Lists;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+
+import it.smartcommunitylab.aac.dto.BasicProfile;
+import it.smartcommunitylab.aac.manager.BasicProfileManager;
+import it.smartcommunitylab.aac.manager.RoleManager;
+import it.smartcommunitylab.aac.manager.UserManager;
+import it.smartcommunitylab.aac.model.ClientDetailsEntity;
+import it.smartcommunitylab.aac.model.Role;
+import it.smartcommunitylab.aac.model.User;
+import it.smartcommunitylab.aac.openid.service.JWKSetKeyStore;
+import it.smartcommunitylab.aac.repository.ClientDetailsRepository;
+
+@Service
+public class AACJwtTokenConverter extends JwtAccessTokenConverter {
+    private static final Logger logger = LoggerFactory.getLogger(AACJwtTokenConverter.class);
+
+    @Autowired
+    private JWKSetKeyStore jwtKeyStore;
+
+    @Autowired
+    private ClientDetailsRepository clientRepository;
+
+    @Autowired
+    private UserManager userManager;
+
+    @Autowired
+    private BasicProfileManager profileManager;
+
+    @Autowired
+    private RoleManager roleManager;
+
+    private Map<String, String> customHeaders = new HashMap<>();
+
+    @Value("${security.oauth2.kid}")
+    private String kid;
+
+    @Value("${security.oauth2.key}")
+    private String key;
+
+    // re-declare objects from parent
+    private Signer signer;
+    private AccessTokenConverter tokenConverter = new DefaultAccessTokenConverter();
+    private JsonParser objectMapper = JsonParserFactory.create();
+
+    public AACJwtTokenConverter() {
+        super();
+    }
+
+    @PostConstruct
+    private void init() {
+        logger.info("Initialize JWT tokenConverter");
+
+        // ensure signer is null since superclass initializes it
+        this.signer = null;
+
+        // if set MAC key use it
+        if (!key.isEmpty()) {
+            // default is HMAC
+            this.setSigningKey(key);
+            signer = new MacSigner(key);
+        }
+
+        // if set kid look in keystore for RSA keypair
+        if (!kid.isEmpty() && !jwtKeyStore.getKeys().isEmpty()) {
+            try {
+                // fetch the matching key
+
+                for (JWK jwk : jwtKeyStore.getKeys()) {
+
+                    if (jwk instanceof RSAKey && kid.equals(jwk.getKeyID())) {
+
+                        // derive signer
+                        KeyPair keyPair = ((RSAKey) jwk).toKeyPair();
+                        this.setKeyPair(keyPair);
+                        this.signer = new RsaSigner((RSAPrivateKey) keyPair.getPrivate());
+
+                        // store kid because we need to add it to claims
+                        customHeaders.put("kid", kid);
+                    }
+                }
+            } catch (JOSEException e) {
+                logger.error("Error reading RSA key: " + e.getMessage());
+            }
+
+        }
+    }
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        logger.debug("enhance access token " + accessToken.getTokenType() + " for " + authentication.getName()
+                + " value " + accessToken.toString());
+
+        OAuth2Request request = authentication.getOAuth2Request();
+        // build a new token with correct claims
+        DefaultOAuth2AccessToken result = new DefaultOAuth2AccessToken(accessToken);
+        Map<String, Object> info = new LinkedHashMap<String, Object>(accessToken.getAdditionalInformation());
+
+        logger.debug("previous claims in token:" + info.keySet().toString());
+
+        String tokenId = result.getValue();
+
+        // check if value is already a JWT
+        if (isJwt(tokenId)) {
+            // extract tokenId from jti field
+            Jwt old = JwtHelper.decode(tokenId);
+            tokenId = (String) this.objectMapper.parseMap(old.getClaims()).get(JTI);
+        }
+
+        if (!info.containsKey(TOKEN_ID)) {
+            info.put(TOKEN_ID, tokenId);
+        } else {
+            tokenId = (String) info.get(TOKEN_ID);
+        }
+
+        // duplicate list for claims, we will reuse info in result
+        Map<String, Object> claims = new LinkedHashMap<String, Object>(info);
+
+        // fetch client and reformat token
+        String clientId = request.getClientId();
+        // set only this client as audience for token - also needs fix in encode due to
+        // tokenConverter misbehavior
+        claims.put(AUD, Lists.newArrayList(clientId));
+
+//        // reset additional claims for scopes to a list instead of an array
+//        info.put("scope", String.join(" ", request.getScope()));
+
+        User user = null;
+        try {
+            // fetch from auth
+            Object principal = authentication.getPrincipal();
+            org.springframework.security.core.userdetails.User auth = (org.springframework.security.core.userdetails.User) principal;
+
+            // fetch user from db
+            long userId = Long.parseLong(auth.getUsername());
+            user = userManager.findOne(userId);
+
+        } catch (Exception e) {
+            // user is not available, thus all user claims will fail
+        }
+
+        // add claims for user details if requested via scopes
+        if (user != null) {
+            logger.debug("fetch profile via profilemanager");
+
+            BasicProfile profile = profileManager.getBasicProfileById(user.getId().toString());
+
+            // add basic profile claims as alternatives to "user_name"
+            claims.put("username", profile.getUsername());
+            claims.put("preferred_username", profile.getUsername());
+
+            if (request.getScope().contains("profile")) {
+                claims.put("given_name", profile.getName());
+                claims.put("family_name", profile.getSurname());
+                claims.put("name", profile.getSurname() + " " + profile.getName());
+            }
+
+            if (request.getScope().contains("email")) {
+                claims.put("email", profile.getUsername());
+            }
+
+            if (request.getScope().contains("user.roles.me")) {
+                try {
+                    Set<Role> roles = roleManager.getRoles(user.getId());
+                    if (roles != null) {
+
+                        // build roles list as plain array
+                        Set<String> rolesList = new HashSet<>();
+                        for (Role role : roles) {
+                            rolesList.add(role.getAuthority());
+                        }
+                        claims.put("roles", rolesList.toArray(new String[0]));
+
+                        // build also as realm/resource claims
+                        Set<String> realmRoles = new HashSet<>();
+                        for (Role role : roles) {
+                            if (StringUtils.isEmpty(role.getContext()) && StringUtils.isEmpty(role.getSpace())) {
+                                realmRoles.add(role.getRole());
+                            }
+                        }
+                        net.minidev.json.JSONObject realmRoleObj = new net.minidev.json.JSONObject();
+                        realmRoleObj.put("roles", realmRoles.toArray(new String[0]));
+                        claims.put("realm_access", realmRoleObj);
+
+                        Map<String, Set<String>> resourceRoles = new HashMap<>();
+                        for (Role role : roles) {
+                            // role is context/space:role
+                            if (!StringUtils.isEmpty(role.getContext()) && !StringUtils.isEmpty(role.getSpace())) {
+                                // put as context/space:role
+                                String key = role.getContext() + "/" + role.getSpace();
+                                if (!resourceRoles.containsKey(key)) {
+                                    resourceRoles.put(key, new HashSet<String>());
+                                }
+                                resourceRoles.get(key).add(role.getRole());
+                            } else if (!StringUtils.isEmpty(role.getContext())
+                                    && StringUtils.isEmpty(role.getSpace())) {
+                                // put as context:role
+                                if (!resourceRoles.containsKey(role.getContext())) {
+                                    resourceRoles.put(role.getContext(), new HashSet<String>());
+                                }
+                                resourceRoles.get(role.getContext()).add(role.getRole());
+                            }
+                        }
+
+                        net.minidev.json.JSONObject resourceRolesObj = new net.minidev.json.JSONObject();
+                        for (String res : resourceRoles.keySet()) {
+                            net.minidev.json.JSONObject resObj = new net.minidev.json.JSONObject();
+                            resObj.put("roles", resourceRoles.get(res).toArray(new String[0]));
+                            resourceRolesObj.put(res, resObj);
+                        }
+                        claims.put("resource_access", resourceRolesObj);
+
+                        // also build list of "groups" (as plain array)
+                        // define a group as context+space, ignoring role
+                        Set<String> groups = new HashSet<>();
+                        for (Role role : roles) {
+                            if (!StringUtils.isEmpty(role.getContext()) && !StringUtils.isEmpty(role.getSpace())) {
+                                groups.add(role.getContext() + "/" + role.getSpace());
+                            }
+                        }
+                        claims.put("groups", groups.toArray(new String[0]));
+                    }
+                } catch (Exception rex) {
+                    logger.error("error fetching roles for user " + user.getId());
+                }
+            }
+        }
+
+        // save claims and encode
+        result.setAdditionalInformation(claims);
+        result.setValue(encode(result, authentication));
+
+        // verify and format refresh token
+        OAuth2RefreshToken refreshToken = result.getRefreshToken();
+        if (refreshToken != null) {
+            DefaultOAuth2AccessToken encodedRefreshToken = new DefaultOAuth2AccessToken(accessToken);
+            encodedRefreshToken.setValue(refreshToken.getValue());
+            // Refresh tokens do not expire unless explicitly of the right type
+            encodedRefreshToken.setExpiration(null);
+            try {
+                Map<String, Object> map = objectMapper
+                        .parseMap(JwtHelper.decode(refreshToken.getValue()).getClaims());
+                if (map.containsKey(TOKEN_ID)) {
+                    encodedRefreshToken.setValue(map.get(TOKEN_ID).toString());
+                }
+            } catch (IllegalArgumentException e) {
+            }
+            Map<String, Object> refreshTokenInfo = new LinkedHashMap<String, Object>(
+                    accessToken.getAdditionalInformation());
+            refreshTokenInfo.put(TOKEN_ID, encodedRefreshToken.getValue());
+            refreshTokenInfo.put(ACCESS_TOKEN_ID, tokenId);
+//            // reset additional claims for scopes to a list instead of an array
+//            refreshTokenInfo.put("scope", String.join(" ", request.getScope()));
+            encodedRefreshToken.setAdditionalInformation(refreshTokenInfo);
+
+            DefaultOAuth2RefreshToken token = new DefaultOAuth2RefreshToken(
+                    encode(encodedRefreshToken, authentication));
+            if (refreshToken instanceof ExpiringOAuth2RefreshToken) {
+                Date expiration = ((ExpiringOAuth2RefreshToken) refreshToken).getExpiration();
+                encodedRefreshToken.setExpiration(expiration);
+                token = new DefaultExpiringOAuth2RefreshToken(encode(encodedRefreshToken, authentication), expiration);
+            }
+            result.setRefreshToken(token);
+        }
+
+        // reset additional claims to avoid leaking in json response
+        // they should appear only in JWT, still we need JTI from tokenId
+        result.setAdditionalInformation(info);
+
+        // rewrite token type because we don't want "bearer" lowercase in response...
+        if (result.getTokenType().equals(OAuth2AccessToken.BEARER_TYPE.toLowerCase())) {
+            result.setTokenType(OAuth2AccessToken.BEARER_TYPE);
+        }
+
+        return result;
+    }
+
+    @Override
+    protected String encode(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        logger.debug("encode access token " + accessToken.getTokenType() + " for " + authentication.getName()
+                + " value " + accessToken.toString());
+
+        // convert base claims to map
+        @SuppressWarnings("unchecked")
+        Map<String, Object> claims = (Map<String, Object>) tokenConverter.convertAccessToken(accessToken,
+                authentication);
+
+        OAuth2Request request = authentication.getOAuth2Request();
+
+        // fetch data and reformat token
+        String clientId = request.getClientId();
+        ClientDetailsEntity client = clientRepository.findByClientId(clientId);
+        // reset AUD to only client since token converter HARDCODES it ignoring claims
+        claims.put(AUD, clientId);
+
+        // reset additional claims for scopes to a list instead of an array
+        claims.put("scope", String.join(" ", request.getScope()));
+
+        // handle authorities claim here since token converter HARDCODES it...
+        if (this.isRefreshToken(accessToken)) {
+            // clear authorities from refresh token
+            claims.remove(AUTHORITIES);
+        }
+
+        if (!request.getScope().contains("user.roles.me")) {
+            // clear authorities from access token
+            claims.remove(AUTHORITIES);
+        }
+
+        // rewrite user_name as sub, if requested by scopes we will already have an
+        // username claim
+        if (claims.containsKey("user_name")) {
+            claims.put("sub", claims.get("user_name").toString());
+            claims.remove("user_name");
+        }
+
+        // clear id-token from claims to avoid embedding id_token in access token JWT
+        if (claims.containsKey("id_token")) {
+            claims.remove("id_token");
+        }
+
+        // convert to JSON
+        String content;
+        try {
+            content = this.objectMapper.formatMap(claims);
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot convert access token to JSON", e);
+        }
+
+        // figure out which signer to use:
+        // if RSA keys provided use the same for all
+        // if global key set use singleton HMAC
+        // otherwise use client secret as HMAC key
+        Signer s = getSigner(client);
+
+        String token = JwtHelper.encode(
+                content,
+                s,
+                this.customHeaders).getEncoded();
+        logger.debug("encoded jwt token " + token.toString());
+
+        return token;
+    }
+
+    @Override
+    public Map<String, ?> convertAccessToken(OAuth2AccessToken token, OAuth2Authentication authentication) {
+        logger.debug("convert access token " + token.toString());
+        return super.convertAccessToken(token, authentication);
+    }
+
+    @Override
+    public OAuth2AccessToken extractAccessToken(String value, Map<String, ?> map) {
+        logger.debug("extract access token " + value);
+        return super.extractAccessToken(value, map);
+    }
+
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
+        logger.debug("extract authentication " + map.toString());
+        return super.extractAuthentication(map);
+    }
+
+    @Override
+    protected Map<String, Object> decode(String token) {
+        logger.debug("decode token " + token);
+        return super.decode(token);
+    }
+
+    private boolean isJwt(String value) {
+        // simply check for format header.body.signature
+        int firstPeriod = value.indexOf('.');
+        int lastPeriod = value.lastIndexOf('.');
+
+        if (firstPeriod <= 0 || lastPeriod <= firstPeriod) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private Signer getSigner(ClientDetailsEntity client) {
+        if (this.signer != null) {
+            // always use global if defined
+            return this.signer;
+        } else {
+            // can't reuse since client secret keys could change
+            return new MacSigner(client.getClientSecret());
+        }
+    }
+
+}
