193a795c32081689c2e20ad21720e268813b7c13
ffais <ffais@fbk.eu>
2019-12-03 09:59:45 +0100

    new dockerfile version

--- init.sh
+++ init.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-if [ -z "${APIM_HOSTNAME}" ]; then
-  echo "APIM_HOSTNAME is not set"
-else
-  rm -f cert.pem && echo -n | openssl s_client -connect ${APIM_HOSTNAME}:9443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ./cert.pem
-  keytool -import -trustcacerts -file cert.pem -alias root -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -noprompt
-fi
-exec java -XX:MaxRAMFraction=2 -jar -Dspring.config.location=/home/aac/application-local.yml -Dspring.profiles.active=local,authorization aac.jar
+
+exec java -XX:MaxRAMFraction=2 -jar aac.jar
