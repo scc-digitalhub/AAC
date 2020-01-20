07a2d7fd1e4c9e00464a35c2fa7cff344cf54f44
Matteo Saloni <matteo.saloni@gmail.com>
2020-01-20 13:34:20 +0100

    update claims to match openID spec, remove email from basic profile

--- src/main/java/it/smartcommunitylab/aac/Config.java
+++ src/main/java/it/smartcommunitylab/aac/Config.java
@@ -64,6 +64,8 @@ public class Config {
 
 	/** Open ID connect scope */
 	public static final String SCOPE_OPENID = "openid";
+	/** Open Id offline access (refresh token) **/
+	public static final String SCOPE_OFFLINE_ACCESS = "offline_access";	
 	/** basic scopes */
 	public static final String SCOPE_PROFILE = "profile";
     public static final String SCOPE_EMAIL = "email";
