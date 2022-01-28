b6cf0d1872abcbddd904e93fed9c6b2e97a7a19c
civts <gzanolli@fbk.eu>
2022-01-28 11:09:10 +0000

    Align WebAuthnAuthenticationException w/ internal
    InternalAuthenticationException

--- src/main/java/it/smartcommunitylab/aac/webauthn/auth/WebAuthnAuthenticationException.java
+++ src/main/java/it/smartcommunitylab/aac/webauthn/auth/WebAuthnAuthenticationException.java
@@ -1,11 +1,79 @@
 package it.smartcommunitylab.aac.webauthn.auth;
 
+
 import org.springframework.security.core.AuthenticationException;
 
-public class WebAuthnAuthenticationException extends AuthenticationException {
+import it.smartcommunitylab.aac.SystemKeys;
 
-    public WebAuthnAuthenticationException(String msg) {
-        super(msg);
+public class WebAuthnAuthenticationException extends AuthenticationException {
+ 
+    private static final long serialVersionUID = SystemKeys.AAC_CORE_SERIAL_VERSION;
+
+    private final String subject;
+    private final String username;
+    private final String credentials;
+    private final String flow;
+    private final AuthenticationException exception;
+
+    public WebAuthnAuthenticationException(String subject, String message) {
+        super(message);
+        this.subject = subject;
+        this.username = null;
+        this.flow = null;
+        this.credentials = null;
+        this.exception = null;
     }
 
+    public WebAuthnAuthenticationException(String subject, String username, String credentials, String flow,
+            AuthenticationException ex) {
+        super(ex.getMessage(), ex.getCause());
+        this.subject = subject;
+        this.username = username;
+        this.credentials = credentials;
+        this.flow = flow;
+        this.exception = ex;
+    }
+
+    public WebAuthnAuthenticationException(String subject, String username, String credentials, String flow,
+            AuthenticationException ex,
+            String message) {
+        super(message, ex.getCause());
+        this.subject = subject;
+        this.username = username;
+        this.credentials = credentials;
+        this.flow = flow;
+        this.exception = ex;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCredentials() {
+        return credentials;
+    }
+
+    public String getFlow() {
+        return flow;
+    }
+
+    public AuthenticationException getException() {
+        return exception;
+    }
+
+    public String getError() {
+        return exception != null ? exception.getClass().getSimpleName() : null;
+    }
+
+    public String getErrorMessage() {
+        String error = getError();
+        if (error == null) {
+            return "internal_error";
+        }
+        return "error." + error;
+    } 
 }
