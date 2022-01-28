9f17b458d62b707ef43ba155e18bc32eabb1498a
civts <gzanolli@fbk.eu>
2022-01-28 11:21:21 +0000

    Extract AssertionRequestSerializer in a new class

--- src/main/java/it/smartcommunitylab/aac/webauthn/model/WebAuthnLoginResponse.java
+++ src/main/java/it/smartcommunitylab/aac/webauthn/model/WebAuthnLoginResponse.java
@@ -1,16 +1,11 @@
 package it.smartcommunitylab.aac.webauthn.model;
 
-import java.io.IOException;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.yubico.webauthn.AssertionRequest;
 
 @Valid
@@ -42,23 +37,4 @@ public class WebAuthnLoginResponse {
         this.assertionrequest = assertionrequest;
     }
 
-}
-
-class AssertionRequestSerializer
-        extends StdSerializer<AssertionRequest> {
-    private static final long serialVersionUID = 1L;
-
-    public AssertionRequestSerializer() {
-        this(null);
-    }
-
-    public AssertionRequestSerializer(Class<AssertionRequest> t) {
-        super(t);
-    }
-
-    @Override
-    public void serialize(AssertionRequest value,
-            JsonGenerator generator, SerializerProvider arg2) throws IOException {
-        generator.writeRawValue(value.toCredentialsGetJson());
-    }
 }
