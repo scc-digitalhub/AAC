55b7a9575f4893a089ceb28b408f077410bb070f
civts <gzanolli@fbk.eu>
2022-02-03 14:07:34 +0000

    Define DTOs for ceremony initial requests

---
+++ src/main/java/it/smartcommunitylab/aac/webauthn/model/WebAuthnRegistrationStartRequest.java
@@ -0,0 +1,35 @@
+package it.smartcommunitylab.aac.webauthn.model;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Valid
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WebAuthnRegistrationStartRequest {
+
+    @JsonProperty("username")
+    @NotNull
+    private String username;
+
+    @JsonProperty("displayName")
+    private String displayName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
