ba9d7c0b212b0878e810af323fb2559d991efa05
Matteo Saloni <matteo.saloni@gmail.com>
2022-08-22 17:19:51 +0200

    update monitoring security config to filter per path AND per port, avoids evaluating the filter on every request and removes the dummyrequest match on api errors

--- src/main/java/it/smartcommunitylab/aac/config/MonitoringSecurityConfig.java
+++ src/main/java/it/smartcommunitylab/aac/config/MonitoringSecurityConfig.java
@@ -1,5 +1,10 @@
 package it.smartcommunitylab.aac.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +14,10 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /*
@@ -24,6 +33,12 @@ public class MonitoringSecurityConfig {
     @Value("${management.server.port}")
     private int managementPort;
 
+    @Value("${management.endpoints.web.exposure.include}")
+    private String[] endpoints;
+
+    @Value("${management.endpoints.web.base-path}")
+    private String basePath;
+
     /*
      * Configure a separated security context for management
      */
@@ -31,13 +46,27 @@ public class MonitoringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // match only actuator endpoints
-        http.requestMatcher(forPort(managementPort))
+        http.requestMatcher(getRequestMatcher())
                 .authorizeRequests((requests) -> requests.anyRequest().permitAll())
+                .exceptionHandling()
+                // use 403
+                .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
                 // we don't want a session for these endpoints
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
+    }
+
+    public RequestMatcher getRequestMatcher() {
+        List<RequestMatcher> endpointMatchers = Arrays.stream(endpoints)
+                .map(u -> new AntPathRequestMatcher(basePath + "/" + u))
+                .collect(Collectors.toList());
+        List<RequestMatcher> antMatchers = new ArrayList<>(endpointMatchers);
+        antMatchers.add(new AntPathRequestMatcher(basePath + "/"));
+
+        return new AndRequestMatcher(new OrRequestMatcher(antMatchers), forPort(managementPort));
     }
 
     private RequestMatcher forPort(final int port) {
