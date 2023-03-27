0d9d0d48e8bf2d3e121d4c1dc3676fcc2ebdf5df
Gian Paolo Rossi <gianpaolo.rossi@tndigit.it>
2023-03-27 13:50:12 +0200

    fix: check for the ADMIN authority not for a role in system

--- src/main/resources/public/js/realmapps.js
+++ src/main/resources/public/js/realmapps.js
@@ -409,8 +409,8 @@ angular.module('aac.controllers.realmapps', [])
         var slug = $stateParams.realmId;
         var clientId = $stateParams.clientId;
 
-        var isSystemRealmAdmin = $rootScope.user.authorities.some(a => (a.realm === "system" && a.role === 'ROLE_ADMIN'));
-        $scope.isRealmAdmin = isSystemRealmAdmin || $rootScope.user.authorities.some(a => (a.realm === slug && a.role === 'ROLE_ADMIN'));
+        $scope.isAdmin = $rootScope.user.authorities.some(a => (a.authority === 'ROLE_ADMIN'));
+        $scope.isRealmAdmin = $scope.isAdmin || $rootScope.user.authorities.some(a => (a.realm === slug && a.role === 'ROLE_ADMIN'));
         $scope.isRealmDev = $scope.isRealmAdmin || $rootScope.user.authorities.some(a => (a.realm === slug && a.role === 'ROLE_DEVELOPER'));
 
         $scope.clientView = 'overview';
