0c3bcedca2681b9b7715554bd7f811c86ec2a4ec
Gian Paolo Rossi <gianpaolo.rossi@tndigit.it>
2023-03-27 12:13:53 +0200

    This is a combination of 3 commits.
    fix #330: add possibility to change authorities for APPs as SuperADMIN cross any realms
    
    add possibility to change authorities for APPs as SuperADMIN cross any realms
    
    fix: check for the ADMIN authority not for a role in system

--- src/main/resources/public/js/realmapps.js
+++ src/main/resources/public/js/realmapps.js
@@ -409,7 +409,8 @@ angular.module('aac.controllers.realmapps', [])
         var slug = $stateParams.realmId;
         var clientId = $stateParams.clientId;
 
-        $scope.isRealmAdmin = $rootScope.user.authorities.some(a => (a.realm === slug && a.role === 'ROLE_ADMIN'));
+        $scope.isAdmin = $rootScope.user.authorities.some(a => (a.authority === 'ROLE_ADMIN'));
+        $scope.isRealmAdmin = $scope.isAdmin || $rootScope.user.authorities.some(a => (a.realm === slug && a.role === 'ROLE_ADMIN'));
         $scope.isRealmDev = $scope.isRealmAdmin || $rootScope.user.authorities.some(a => (a.realm === slug && a.role === 'ROLE_DEVELOPER'));
 
         $scope.clientView = 'overview';
