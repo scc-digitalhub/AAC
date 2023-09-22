6dfda1084c7cc2871fa5733233df539029ea3e3f
Nawaz Khurshid <nawaz1981@gmail.com>
2023-09-22 15:14:03 +0200

    Added 'CustomDeleteButton' with 'CustomDeleteConfirmation' dialog

---
+++ dev-console/src/components/CustomDeleteConfirm.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { useCallback, MouseEventHandler, useState, useEffect } from 'react';
+import PropTypes, { ReactComponentLike } from 'prop-types';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import { alpha } from '@mui/material/styles';
+import ActionDelete from '@mui/icons-material/Delete';
+import AlertError from '@mui/icons-material/ErrorOutline';
+import clsx from 'clsx';
+import { useTranslate } from 'ra-core';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+/**
+ * Custom Confirmation dialog
+ *
+ * @example
+ * <Confirm
+ *     isOpen={true}
+ *     title="Delete Item"
+ *     content="Are you sure you want to delete this item?"
+ *     confirm="Yes"
+ *     deleteColor="danger"
+ *     ConfirmIcon=ActionCheck
+ *     CancelIcon=AlertError
+ *     cancel="Cancel"
+ *     onDelete={() => { // do something }}
+ *     onClose={() => { // do something }}
+ * />
+ */
+export const CustomDeleteConfirm = (props: any) => {
+    const {
+        className,
+        isOpen = false,
+        loading,
+        title,
+        content,
+        cancel = 'ra.action.cancel',
+        deleteColor = 'danger',
+        DeleteIcon = ActionDelete,
+        CancelIcon = AlertError,
+        onClose,
+        onDelete,
+        disableDeleteButton,
+        translateOptions = {},
+        ...rest
+    } = props;
+
+    const translate = useTranslate();
+
+    const handleDelete = useCallback(
+        (e: { stopPropagation: () => void }) => {
+            e.stopPropagation();
+            onDelete(e);
+        },
+        [onDelete]
+    );
+
+    const handleClick = useCallback((e: { stopPropagation: () => void }) => {
+        e.stopPropagation();
+    }, []);
+
+    return (
+        <Dialog
+            className={className}
+            open={isOpen}
+            onClose={onClose}
+            onClick={handleClick}
+            aria-labelledby="alert-dialog-title"
+            {...rest}
+        >
+            <DialogTitle id="alert-dialog-title">
+                {typeof title === 'string'
+                    ? translate(title, { _: title, ...translateOptions })
+                    : title}
+            </DialogTitle>
+            <DialogContent>
+                {typeof content === 'string' ? (
+                    <DialogContentText>
+                        {translate(content, {
+                            _: content,
+                            ...translateOptions,
+                        })}
+                    </DialogContentText>
+                ) : (
+                    content
+                )}
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    disabled={loading}
+                    onClick={onClose}
+                    startIcon={<CancelIcon />}
+                >
+                    {translate(cancel, { _: cancel })}
+                </Button>
+                <Button
+                    disabled={disableDeleteButton}
+                    onClick={handleDelete}
+                    autoFocus
+                    sx={{ color: 'red' }}
+                    startIcon={<DeleteIcon />}
+                >
+                    Delete
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
