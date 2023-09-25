c46bf8c825cbdf2391a5ebf196fba0d6d443a254
Nawaz Khurshid <nawaz1981@gmail.com>
2023-09-25 11:34:02 +0200

    Added app creation functionality

--- dev-console/src/apps/index.tsx
+++ dev-console/src/apps/index.tsx
@@ -1,7 +1,9 @@
+import { AppCreate } from './AppCreate';
 import { AppList } from './AppList';
 import { AppShow } from './AppShow';
 
 export default {
     list: AppList,
-    show: AppShow
+    show: AppShow,
+    create: AppCreate,
 };
