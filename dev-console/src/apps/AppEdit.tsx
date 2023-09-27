1b413cbc1d738d40669d7fb9be9729f818047bcf
Nawaz Khurshid <nawaz1981@gmail.com>
2023-09-27 11:51:04 +0200

    Added 'App Edit' functionality

--- dev-console/src/apps/AppEdit.tsx
+++ dev-console/src/apps/AppEdit.tsx
@@ -9,13 +9,19 @@ import {
     Typography,
 } from '@mui/material';
 import {
+    ArrayInput,
+    BooleanInput,
     Button,
     Edit,
     EditBase,
     Form,
+    NumberInput,
     RichTextField,
     SaveButton,
+    SelectArrayInput,
+    SelectInput,
     ShowBase,
+    SimpleFormIterator,
     SimpleShowLayout,
     TabbedShowLayout,
     TextField,
@@ -56,10 +62,10 @@ const AppTabComponent = () => {
     return (
         <>
             <br />
-            <Typography variant="h5" sx={{ mr: 2, mt: 1 }}>
+            <Typography variant="h5" sx={{ ml: 2, mt: 1 }}>
                 <StarBorderIcon color="primary" /> {record.name}
             </Typography>
-            <Typography variant="h6" sx={{ mr: 2 }}>
+            <Typography variant="h6" sx={{ ml: 2 }}>
                 {record.id}
             </Typography>
             <br />
@@ -71,13 +77,200 @@ const AppTabComponent = () => {
                     <EditSetting />
                 </TabbedShowLayout.Tab>
                 <TabbedShowLayout.Tab label="OAuth2">
-                    <RichTextField source="body" label={false} />
+                    <Typography variant="h5" sx={{ mr: 2 }}>
+                        OAuth2.0 Configuration
+                    </Typography>
+                    <Typography variant="h6" sx={{ mr: 2 }}>
+                        Basic client configuration for OAuth2/OpenId Connect
+                    </Typography>
+                    <TextField source="clientId" />
+                    <EditOAuthSetting />
                 </TabbedShowLayout.Tab>
             </TabbedShowLayout>
         </>
     );
 };
 
+const EditOAuthSetting = () => {
+    const params = useParams();
+    const options = { meta: { realmId: params.realmId } };
+    const notify = useNotify();
+    const refresh = useRefresh();
+    const { isLoading, record } = useEditContext<any>();
+    if (isLoading || !record) return null;
+    const onSuccess = (data: any) => {
+        notify(`App updated successfully`);
+        refresh();
+    };
+    return (
+        <EditBase
+            mutationMode="pessimistic"
+            mutationOptions={{ ...options, onSuccess }}
+            queryOptions={options}
+        >
+            <Form>
+                <Card>
+                    <CardContent>
+                        <Box>
+                            <Box display="flex">
+                                <Box flex="1" mt={-1}>
+                                    <Box display="flex" width={430}>
+                                        <SelectArrayInput
+                                            source="configuration.authenticationMethods"
+                                            choices={[
+                                                {
+                                                    id: 'client_secret_post',
+                                                    name: 'client_secret_post',
+                                                },
+                                                {
+                                                    id: 'private_key_jwt',
+                                                    name: 'private_key_jwt',
+                                                },
+                                                {
+                                                    id: 'client_secret_basic',
+                                                    name: 'client_secret_basic',
+                                                },
+                                                {
+                                                    id: 'client_secret_jwt',
+                                                    name: 'client_secret_jwt',
+                                                },
+                                                {
+                                                    id: 'none',
+                                                    name: 'none',
+                                                },
+                                            ]}
+                                        />
+                                    </Box>
+                                    <Box display="flex" width={430}>
+                                        <SelectArrayInput
+                                            source="configuration.authorizedGrantTypes"
+                                            choices={[
+                                                {
+                                                    id: 'authorization_code',
+                                                    name: 'authorization_code',
+                                                },
+                                                {
+                                                    id: 'implicit',
+                                                    name: 'implicit',
+                                                },
+                                                {
+                                                    id: 'refresh_token',
+                                                    name: 'refresh_token',
+                                                },
+                                                {
+                                                    id: 'password',
+                                                    name: 'password',
+                                                },
+                                                {
+                                                    id: 'client_credentials',
+                                                    name: 'client_credentials',
+                                                },
+                                            ]}
+                                        />
+                                    </Box>
+                                    <Box>
+                                        <ArrayInput source="configuration.redirectUris">
+                                            <SimpleFormIterator
+                                                inline
+                                                disableReordering
+                                            >
+                                                <TextInput
+                                                    source=""
+                                                    helperText={false}
+                                                />
+                                            </SimpleFormIterator>
+                                        </ArrayInput>
+                                    </Box>
+                                    <Divider />
+                                    <Typography variant="h5" sx={{ mr: 2 }}>
+                                        Advanced Configuration
+                                    </Typography>
+                                    <Typography variant="h6" sx={{ mr: 2 }}>
+                                        OAuth2 advanced client configuration
+                                    </Typography>
+                                    <Box display="flex" width={430}>
+                                        <SelectInput
+                                            source="configuration.applicationType"
+                                            label="Application type"
+                                            choices={[
+                                                {
+                                                    id: 'web',
+                                                    name: 'Web',
+                                                },
+                                                {
+                                                    id: 'native',
+                                                    name: 'Native',
+                                                },
+                                                {
+                                                    id: 'machine',
+                                                    name: 'Machine',
+                                                },
+                                                {
+                                                    id: 'spa',
+                                                    name: 'SPA',
+                                                },
+                                                {
+                                                    id: 'introspection',
+                                                    name: 'Introspection',
+                                                },
+                                            ]}
+                                        />
+                                    </Box>
+                                    <Box>
+                                        <BooleanInput source="configuration.firstParty" />
+                                    </Box>
+                                    <Box>
+                                        <BooleanInput source="configuration.idTokenClaims" />
+                                    </Box>
+                                    <Box>
+                                        <BooleanInput source="configuration.refreshTokenRotation" />
+                                    </Box>
+                                    <Box display="flex" width={430}>
+                                        <SelectInput
+                                            source="configuration.subjectType"
+                                            label="Subject type"
+                                            choices={[
+                                                {
+                                                    id: 'public',
+                                                    name: 'Public',
+                                                },
+                                                {
+                                                    id: 'pairwise',
+                                                    name: 'Pairwise',
+                                                },
+                                            ]}
+                                        />
+                                    </Box>
+                                    <Box display="flex" width={430}>
+                                        <SelectInput
+                                            source="configuration.tokenType"
+                                            label="Token type"
+                                            choices={[
+                                                {
+                                                    id: 'jwt',
+                                                    name: 'JWT',
+                                                },
+                                                {
+                                                    id: 'opaque',
+                                                    name: 'Opaque',
+                                                },
+                                            ]}
+                                        />
+                                    </Box>
+                                    <Box>
+                                        <NumberInput source="configuration.accessTokenValidity" />
+                                        <NumberInput source="configuration.refreshTokenValidity" />
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </CardContent>
+                    <EditSettingToolbar />
+                </Card>
+            </Form>
+        </EditBase>
+    );
+};
 const EditSetting = () => {
     const params = useParams();
     const options = { meta: { realmId: params.realmId } };
