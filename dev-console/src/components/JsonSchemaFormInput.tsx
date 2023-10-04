71a5293376be0b7fed11e6e7be7afca2260398e7
Nawaz Khurshid <nawaz1981@gmail.com>
2023-10-04 09:57:21 +0200

    Fixes for json schema form component

--- dev-console/src/components/JsonSchemaFormInput.tsx
+++ dev-console/src/components/JsonSchemaFormInput.tsx
@@ -23,8 +23,6 @@ export const JSONSchemaFormInput = (props: JSONSchemaFormatInputProps) => {
         formState: { isSubmitted },
         isRequired,
     } = useInput({
-        // Pass the event handlers to the hook but not the component as the field property already has them.
-        // useInput will call the provided onChange and onBlur in addition to the default needed by react-hook-form.
         onChange,
         onBlur,
         ...props,
@@ -37,24 +35,8 @@ export const JSONSchemaFormInput = (props: JSONSchemaFormatInputProps) => {
         source,
     };
 
-    const formContext = useFormContext();
-    let updatedData = {};
-
-    // moved the setValue into a useEffect
-    useEffect(() => {
-        // if (updatedData) {
-        // formContext.setValue('configuration', updatedData);
-        // }
-    });
-
-    // if (field.value) {
-    //     updatedData = field.value;
-    // }
-
     const update = (data: any) => {
-        // updatedData = JSON.parse(type);
         field.onChange(data);
-        return console.log.bind(console, data);
     };
 
     return (
