c4a02494ab251ec06c80576b773e925649fb7987
Matteo Saloni <matteo.saloni@gmail.com>
2023-11-13 16:35:31 +0100

    user context simple tests

---
+++ src/test/java/it/smartcommunitylab/aac/users/UserContextTests.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2023 Fondazione Bruno Kessler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.smartcommunitylab.aac.users;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import it.smartcommunitylab.aac.accounts.model.UserAccount;
+import it.smartcommunitylab.aac.internal.model.InternalUserAccount;
+import it.smartcommunitylab.aac.users.model.User;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringJUnitWebConfig
+public class UserContextTests {
+
+    @Test
+    public void userContextTest() throws Exception {
+        User user = new User("user-id", "test");
+
+        assertThat(user.getResources()).isNotNull();
+    }
+
+    @Test
+    public void accountContextTest() throws Exception {
+        User user = new User("user-id", "test");
+        InternalUserAccount account = new InternalUserAccount("internal", "internal", "test", "account-id");
+        account.setUserId("user-id");
+
+        assertThat(user.getAccounts()).isEmpty();
+        user.setAccounts(Collections.singletonList(account));
+
+        assertThat(user.getAccounts()).isNotEmpty();
+        assertThat(user.getAccounts()).contains(account);
+    }
+}
