88e864dfa9efdab9fbdfea65ed76760c5b7d17a4
Nawaz Khurshid <nawaz1981@gmail.com>
2023-11-20 15:24:29 +0100

    Added persistence + service integration

--- src/main/java/it/smartcommunitylab/aac/files/controller/FilesController.java
+++ src/main/java/it/smartcommunitylab/aac/files/controller/FilesController.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2023 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package it.smartcommunitylab.aac.files.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
@@ -22,7 +37,6 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import it.smartcommunitylab.aac.files.message.ResponseMessage;
-import it.smartcommunitylab.aac.files.model.FileInfo;
 import it.smartcommunitylab.aac.files.persistence.FileDB;
 import it.smartcommunitylab.aac.files.service.FilesStorageService;
 
@@ -48,12 +62,8 @@ public class FilesController {
 	}
 
 	@GetMapping("/list")
-	public ResponseEntity<List<FileInfo>> getListFiles() {
-		List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
-			String filename = path.getFileName().toString();
-			return new FileInfo(filename, null);
-		}).collect(Collectors.toList());
-
+	public ResponseEntity<List<FileDB>> getListFiles() {
+		List<FileDB> fileInfos = storageService.loadAll();
 		return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
 	}
 
