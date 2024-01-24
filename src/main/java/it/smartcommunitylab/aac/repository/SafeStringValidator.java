c732241e3a9985c3bf97f83b76a5c09f3402798d
Matteo Saloni <matteo.saloni@gmail.com>
2024-01-24 17:53:51 +0100

    cleanup SafeString implementation + add validators for collections,maps

--- src/main/java/it/smartcommunitylab/aac/repository/SafeStringValidator.java
+++ src/main/java/it/smartcommunitylab/aac/repository/SafeStringValidator.java
@@ -3,16 +3,15 @@ package it.smartcommunitylab.aac.repository;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import org.jsoup.Jsoup;
-import org.jsoup.safety.Safelist;
 import org.springframework.util.StringUtils;
 
 public class SafeStringValidator implements ConstraintValidator<SafeString, String> {
 
-    private SafeString safestring;
+    private SafeString constraint;
 
     @Override
-    public void initialize(SafeString safestring) {
-        this.safestring = safestring;
+    public void initialize(SafeString constraint) {
+        this.constraint = constraint;
     }
 
     @Override
@@ -20,13 +19,7 @@ public class SafeStringValidator implements ConstraintValidator<SafeString, Stri
         if (!StringUtils.hasText(s)) {
             return true;
         }
-        return switch (safestring.safelist()) {
-            case NONE -> Jsoup.isValid(s, Safelist.none());
-            case BASIC -> Jsoup.isValid(s, Safelist.basic());
-            case RELAXED -> Jsoup.isValid(s, Safelist.relaxed());
-            case SIMPLE_TEXT -> Jsoup.isValid(s, Safelist.simpleText());
-            case BASIC_WITH_IMAGES -> Jsoup.isValid(s, Safelist.basicWithImages());
-            default -> false;
-        };
+
+        return Jsoup.isValid(s, constraint.safelist().getValue());
     }
 }
