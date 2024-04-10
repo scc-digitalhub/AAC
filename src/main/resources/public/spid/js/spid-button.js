0eb46a3baf0fc031f26b43c5d2a491fb0c28e0c9
td674 <elisa.nicolussi@tndigit.it>
2024-04-10 10:10:35 +0200

    wip: spid button

--- src/main/resources/public/spid/js/spid-button.js
+++ src/main/resources/public/spid/js/spid-button.js
@@ -1,8 +1,9 @@
 function closeOpenItems(provider) {
-  let openMenus = document.querySelectorAll(".spid-idp-button");
-  openMenus.forEach(function(openMenu) {
-    if (!openMenu.id.includes(provider)){
-      openMenu.classList.remove("spid-idp-button-open");
+  // close the dropdown if the user clicks on another dropdown
+  let dropdowns = document.querySelectorAll(".spid-idp-button");
+  dropdowns.forEach(function(openDropdown) {
+    if (!openDropdown.id.includes(provider)){
+      openDropdown.classList.remove("spid-idp-button-open");
     }
   });
 }
@@ -14,14 +15,11 @@ function expandDropdown(provider) {
 }
 
 window.onclick = function (event) {
-  // close the dropdown is the user clicks outside of it
-  if (!event.target.matches(".spid-display-button")) {
-    let dropdowns = document.getElementsByClassName("spid-idp-button");
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("spid-idp-button-open")) {
-        openDropdown.classList.remove("spid-idp-button-open");
-      }
-    }
+  // close the dropdown if the user clicks outside of it
+  if (!event.target.closest(".button-spid")) {
+    let dropdowns = document.querySelectorAll(".spid-idp-button");
+    dropdowns.forEach(function(openDropdown) {
+      openDropdown.classList.remove("spid-idp-button-open");
+    });
   }
 };
