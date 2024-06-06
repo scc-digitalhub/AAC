a743709844342a83556a47d8d0e02668d3eba07e
Matteo Saloni <matteo.saloni@gmail.com>
2024-06-06 11:32:32 +0200

    refactor: inline forked lib + cleanups and updates

---
+++ src/main/java/org/springframework/security/oauth2/common/exceptions/UnapprovedClientAuthenticationException.java
@@ -0,0 +1,20 @@
+package org.springframework.security.oauth2.common.exceptions;
+
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+
+/**
+ * <p>
+ *
+ * @author Ryan Heaton
+ */
+@SuppressWarnings("serial")
+public class UnapprovedClientAuthenticationException extends InsufficientAuthenticationException {
+
+  public UnapprovedClientAuthenticationException(String msg) {
+    super(msg);
+  }
+
+  public UnapprovedClientAuthenticationException(String msg, Throwable t) {
+    super(msg, t);
+  }
+}
